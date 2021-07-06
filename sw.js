c8e7a21fd7c5f1a6b4417b412ff7ee21873a1145
rafael my master <sorafaelbook@gmail.com>
2021-07-06 10:44:15 +0400

    applying sw

---
+++ sw.js
@@ -0,0 +1,39 @@
+const myCache = 'rafCache';
+const toCache = ['index.html', '.', '/javascript/app.js', '/css/styles.css',
+'/images/bpone.jpg',
+'/images/bpthree.jpg',
+'/images/bptwo.jpg',
+'/images/frontpage.svg',
+'/images/rafaelsoimg.png',]
+
+self.addEventListener('install', e => {
+    caches.delete(myCache)
+
+    e.waitUntil(
+        caches.open(myCache).then(cache => {
+            cache.addAll(toCache)
+        })
+    )
+})
+
+
+self.addEventListener('activation', e => {
+
+    e.waitUntil(
+        caches.keys().then(allCaches => {
+            allCaches.map(cacheName => {
+                if(cacheName !== myCache){
+                    return caches.delete(cacheName)
+                }
+            })
+        })
+    )
+})
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        fetch(e.request).catch( () => {
+            return caches.match(e.request)
+        })
+    )
+})
